972bd375df915b24e05bc797ae2242af4f309c64
William Papantoniou <wpapantoniou.biz@gmail.com>
2021-12-04 01:28:00 +1100

    added code to fix mutation inorder to prevent over fitting

--- src/crossover_mutation.py
+++ src/crossover_mutation.py
@@ -16,9 +16,9 @@ def crossover_mutation(parents, pop_size, noi):
             crossover_mutated_dict["member_"+str(counter)] = {"values": parents["member_"+str(first)]["values"][:(slice_amount)] + parents["member_"+str(second)]["values"][-(slice_amount+1):], "fitness": 0}
             counter += 1
 
-    """internal_counter = 0
+    internal_counter = 0
     for member in crossover_mutated_dict:
-        for item in crossover_mutated_dict[member]:
+        for item in crossover_mutated_dict[member]["values"]:
             mutation = random.randint(1,10000)
             if mutation == 1:
                 if item == 1:
@@ -28,6 +28,6 @@ def crossover_mutation(parents, pop_size, noi):
                     crossover_mutated_dict[member][internal_counter] = 1
             
             internal_counter += 1
-        internal_counter = 0"""
+        internal_counter = 0
 
     return(crossover_mutated_dict)
